2253f053c86d6b2d0711c294623ff1841294f4f0
louisljh-wb23-stack <louisljh-wb23@student.tarc.edu.my>
2026-04-16 19:12:29 +0800

    Change title from 'K I Y I' to 'What is your information?'

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,7 +122,7 @@ else:
 
     st.markdown(main_css, unsafe_allow_html=True)
 
-    st.title("K I Y I")
+    st.title("What is your information?")
 
     age = st.number_input("Age", 0, 100, 20)
     sessions = st.number_input("Sessions Per Week", 0, 50, 5)
